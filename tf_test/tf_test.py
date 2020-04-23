cd741010a7e78daacc65a84bf25514e8d16506dc
evanc130 <evan.cusato@students.olin.edu>
2020-04-22 21:56:28 -0400

    lsim works
    that took too long

--- tf_test/tf_test.py
+++ tf_test/tf_test.py
@@ -2,6 +2,9 @@ import numpy as np
 from scipy import signal
 import matplotlib.pyplot as plt
 
+t = np.arange(0.0,10.0,0.01)
+step = 2*np.heaviside(t,1)
+
 name = 'Problem 1C'
 num = [1]
 p1 = np.poly1d([1,1])
@@ -9,8 +12,9 @@ p2 = np.poly1d([1,1,1])
 den = p1*p2
 print(den)
 sys = signal.TransferFunction(num,den)
-t,y = signal.step(sys,T=t)
+tout,y,x = signal.lsim(sys, step, t)
 plt.figure(3)
 plt.plot(t,y,'k-',label=name)
 plt.legend(loc='best')
 plt.show()
+
