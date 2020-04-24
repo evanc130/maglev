6cebe424706ae1343f0a1f66dc117f8f56043501
evanc130 <evan.cusato@students.olin.edu>
2020-04-24 10:28:09 -0400

    I think it works
    the operating point (r_bar) is now a negative number
    the initial position is now expressed as the difference between r and r_bar

--- maglev_sim/maglev_sim.py
+++ maglev_sim/maglev_sim.py
@@ -6,9 +6,10 @@ g = 9.8                                 #[m/s^2]
 gamma = 1                             #[who knows?]
 v_bar = 6                               #[V]
 r_bar = -np.sqrt((gamma*v_bar**2)/g)     #[length]
+print('r_bar = ',r_bar)
+R_hat_0 = 1
 
-
-t = np.arange(0.0,10.0,0.01)
+t = np.arange(0.0,1.0,0.01)
 step = v_bar*np.heaviside(t,1)
 
 name = 'Test'
@@ -18,7 +19,8 @@ den = [1, 0, p1]
 sys = signal.TransferFunction(num,den)
 z,p,k = signal.tf2zpk(num,den)
 print(p)
-tout,y,x = signal.lsim(sys, step, t)
+tout,y,x = signal.lsim(sys, step, t, R_hat_0)
+print(y)
 plt.figure(1)
 plt.plot(t,y,'k-',label=name)
 plt.legend(loc='best')
