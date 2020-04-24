b17e7e012d1fed40dc204a8c1449e95fcb3fa176
evanc130 <evan.cusato@students.olin.edu>
2020-04-23 21:29:17 -0400

    still facing the wrong way

--- maglev_sim/maglev_sim.py
+++ maglev_sim/maglev_sim.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 g = 9.8                                 #[m/s^2]
 gamma = 1                             #[who knows?]
 v_bar = 6                               #[V]
-r_bar = np.sqrt((gamma*v_bar**2)/g)     #[length]
+r_bar = -np.sqrt((gamma*v_bar**2)/g)     #[length]
 
 
 t = np.arange(0.0,10.0,0.01)
@@ -16,6 +16,8 @@ num = 2*gamma/(r_bar**2)
 p1 = 2*gamma*v_bar**2/r_bar**3
 den = [1, 0, p1]
 sys = signal.TransferFunction(num,den)
+z,p,k = signal.tf2zpk(num,den)
+print(p)
 tout,y,x = signal.lsim(sys, step, t)
 plt.figure(1)
 plt.plot(t,y,'k-',label=name)
