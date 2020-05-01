6ca4bb1fc4cc1405baa806ea20f365c7f5f0ea3e
hkolano <hannah.kolano@students.olin.edu>
2020-04-30 21:26:18 -0500

    maglev_simControl.py -> uses python control
    Plots pole zero plot, has gain on controller

--- maglev_sim/maglev_sim.py
+++ maglev_sim/maglev_sim.py
@@ -8,7 +8,7 @@ v_bar = 6                               #[V]
 r_bar = -np.sqrt((gamma*v_bar**2)/g)     #[length]
 print('r_bar = ',r_bar)
 #R_hat_0 = [-1, -0.5, -0.25, -0.1, -0.05, 0.05, 0.1, 0.25, 0.4, 0.5, 1, 2]
-R_hat_0 = 1
+R_hat_0 = 0
 print('R_hat_0 = ',R_hat_0)
 t = np.arange(0.0,1.0,0.01)
 step = 0*np.heaviside(t,1)
@@ -29,8 +29,8 @@ C = signal.TransferFunction([Kd, Kp, Ki],1)
 #print(C)
 plt.figure(1)
 #for i in R_hat_0:
-CG = C*G
-tout,y,x = signal.lsim(CG, step, t, R_hat_0)
+# CG = C*G
+tout,y,x = signal.lsim(G, step, t, R_hat_0)
 print(y)
 plt.plot(t,y)
 #plt.legend(loc='best')
